091d3cf5e712ced8cb71a74c4b370a80fa474b8a
Dillon <10479084@stpsb.pri>
2016-10-07 10:02:59 -0500

    new game

---
+++ websites/platformer/game.js
@@ -0,0 +1,12 @@
+var groundSprites;
+var GROUND_SPRITE_WIDTH = 50;
+var GROUND_SPRITE_HEIGHT =50;
+function setup() {
+    createCanvas(400, 300);
+    background(150, 200, 250);
+    groundSprites = new Group();
+}
+
+function draw() {
+
+}
