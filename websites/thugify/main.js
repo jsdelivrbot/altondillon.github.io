a7fe544c653debfd6b9735147bcf3465044ecdfa
Alton Dillon <Alton Dillon>
2016-10-10 16:14:55 -0500

    made thugify more thugier

--- websites/thugify/main.js
+++ websites/thugify/main.js
@@ -1,5 +1,6 @@
 document.getElementById('go').onclick = function() {
     var inputText = $('#text-input').val();
-    var thugifiedText = inputText + ", dawg";
+    var sayings = ["yo", "dawg", "my nigga"];
+    var thugifiedText = inputText + ", " + sayings[Math.floor(Math.random() * sayings.length)];
     document.getElementById('output').innerHTML = thugifiedText;
 };
