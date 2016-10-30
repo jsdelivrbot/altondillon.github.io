7f4069b7f9ca8275da6a433e27208e609bc9de2a
Alton Dillon <Alton Dillon>
2016-10-29 22:36:22 -0500

    edited website

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 function openNav(){
-    document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("mySidenav").style.width = "250px";
 
 }
 function closeNav(){
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }
