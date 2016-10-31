f6d4bbce7a943b8bebb754b34d6fb676a739316b
Alton Dillon <Alton Dillon>
2016-10-31 18:58:57 -0500

    added more sayings

--- websites/thugify/main.js
+++ websites/thugify/main.js
@@ -1,6 +1,6 @@
 document.getElementById('go').onclick = function() {
     var inputText = $('#text-input').val();
-    var sayings = ["yo", "dawg", "my nigga"];
+    var sayings = ["yo", "dawg", "my nigga", "mofo", "bitch", "ho"];
     var thugifiedText = inputText + ", " + sayings[Math.floor(Math.random() * sayings.length)];
     document.getElementById('output').innerHTML = thugifiedText;
 };
